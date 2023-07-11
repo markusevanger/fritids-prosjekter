ea6f4deceff1e2d650ffc8e21cb68f0a16c4cb9f
markusevanger <markusevanger@gmail.com>
2023-07-11 22:07:45 +0200

    kort script som formatere tekst te jobb avissider spare sann 20 sek

--- jobb/20_sporsmol_formaterer.py
+++ jobb/20_sporsmol_formaterer.py
@@ -5,12 +5,64 @@ def main():
     FIL_NAVN = "20_sporsmol_formaterer.py"
 
 
-    filerListe = os.listdir()
-    filerListe.remove(FIL_NAVN)
-    filerListe.remove(".DS_Store")
+    alleFiler = os.listdir()
+    txtFiler = []
+    for fil in alleFiler:
+        fil_split = os.path.splitext(fil)[1]
+        if fil_split == ".txt":
+            txtFiler.append(fil)
+
     #filerListe.remove("test.txt")
 
-    formaterFil(filerListe[0])
+    grensesnitt(txtFiler)
+
+def grensesnitt(listeAvMuligeFiler):
+
+    dict = {}
+
+    teller = 1
+
+    for x in range(100):
+        print()
+
+
+    print("Blank ENTER for å formatere alle følgende filer.")
+    print("eller skriv tilhørende tall til filen.")
+    print("skriv X for aa avslutte")
+    print()
+
+    for fil in listeAvMuligeFiler:
+        dict[str(teller)] = fil
+        teller+=1
+
+    for key in dict:
+        print(key + ". " + dict[key])
+
+    
+
+    inp = ""
+    while inp != "x" and inp != "X":
+
+        print()
+        inp = input(">>")
+
+        if inp == "":
+            formaterListeAvFiler(listeAvMuligeFiler)
+        elif inp.isnumeric():
+            if int(inp) <= len(dict):
+                formaterFil(dict[inp])
+            else:
+                print("Tallet for høyt/lavt!")
+        elif inp == "x" or inp == "X":
+            print("Avslutter programmet.")
+        else:
+            print("Venligst prøv igjen.")
+    
+    
+
+def formaterListeAvFiler(listeAvFiler):
+    for fil in listeAvFiler:
+        formaterFil(fil)
 
 def formaterFil(tekst_fil):
 
@@ -18,13 +70,13 @@ def formaterFil(tekst_fil):
     fil = open(tekst_fil, "r")
     f = fil.read()
     f = f.replace("\n", "")
-    f = f.replace("20 spørsmål, voksne:", "20 spørsmål, voksne: \n")
-    f = f.replace("Svar:", "\n\nSvar:\n")
+    f = f.replace("20 spørsmål, voksne:", "20 spørsmål, voksne:")
+    f = f.replace("Svar:", "\n\nSvar:")
 
-    print(f)
+    #print(f)
 
     fil.close()
-    fil = open("test.txt", "w")
+    fil = open(tekst_fil, "w")
 
     teller = 0
     while(teller < len(f)):
@@ -37,19 +89,25 @@ def formaterFil(tekst_fil):
 
             if tall != 10:
                 tall = tall - 10
+            if tall == 1:
+                fil.write("\n")
+             
             tall = str(tall)
             fil.write("\n"+tall + ".")
             teller+=3
         
         elif bokstav.isnumeric() and f[teller+1] == "." and not f[teller-1].isnumeric(): #en sifret.
             tall = bokstav + "."
+            if int(bokstav) == 1:
+                fil.write("\n")
             fil.write("\n" + tall)
             teller+=2
         
         else:
             fil.write(bokstav)
             teller+=1
-            
+    
+    print("Ferdig aa formatere " + tekst_fil + ".")
 
     #print(f_ny)
 
